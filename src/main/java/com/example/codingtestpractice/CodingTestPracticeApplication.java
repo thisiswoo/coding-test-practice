ead3dd7d3c333c3a04319e2b99d66ce93927b61c
thisiswoo <thisiswoo0594@gmail.com>
2023-06-13 10:05:09 +0900

    Initial commit

---
+++ src/main/java/com/example/codingtestpractice/CodingTestPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingtestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTestPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codingtestpractice/CodingTestPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingtestpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
