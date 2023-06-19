eef806dd82262ce60a36e033d298d7b91492f073
thisiswoo <thisiswoo0594@gmail.com>
2023-06-19 14:23:33 +0900

    BOJ_2557 - Hello World!

---
+++ src/main/java/com/example/codingtestpractice/category/basic/BOJ_2557.java
@@ -0,0 +1,10 @@
+package com.example.codingtestpractice.category.basic;
+
+/**
+ * [문제] Hello World!를 출력하시오.
+ */
+public class BOJ_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
