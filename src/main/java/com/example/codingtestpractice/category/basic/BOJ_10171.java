8e038446ba2b385cc070b7d596e3c9a212e99ed8
thisiswoo <thisiswoo0594@gmail.com>
2023-06-20 10:43:42 +0900

    BOJ_10171 - 고양이

---
+++ src/main/java/com/example/codingtestpractice/category/basic/BOJ_10171.java
@@ -0,0 +1,18 @@
+package com.example.codingtestpractice.category.basic;
+
+/**
+ * [문제] 아래 예제와 같이 고양이를 출력하시오.
+ * [예제]
+ *      \    /\
+ *       )  ( ')
+ *      (  /  )
+ *       \(__)|
+ */
+public class BOJ_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
