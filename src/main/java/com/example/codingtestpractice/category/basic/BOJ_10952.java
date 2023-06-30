2d94a334cf8a545b34895c776602ef74c8066210
thisiswoo <thisiswoo0594@gmail.com>
2023-06-30 09:45:00 +0900

    BOJ_10952 - A+B - 5 구현 완료

---
+++ src/main/java/com/example/codingtestpractice/category/basic/BOJ_10952.java
@@ -0,0 +1,34 @@
+package com.example.codingtestpractice.category.basic;
+
+import java.util.Scanner;
+
+/**
+ * [A+B - 5]
+ *
+ * [문제] 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ * [입력] 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+ *       각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ *       입력의 마지막에는 0 두 개가 들어온다.
+ * [출력] 각 테스트 케이스마다 A+B를 출력한다.
+ *
+ * 1 1 -> 2
+ * 2 3 -> 5
+ * 3 4 -> 7
+ * 9 8 -> 17
+ * 5 2 -> 7
+ * 0 0 -> 종료
+ */
+public class BOJ_10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            System.out.println(a + b);
+        }
+        sc.close();
+    }
+}
